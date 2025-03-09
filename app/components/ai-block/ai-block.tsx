ccc31608dd03855c650342a95e666ad03e13c86a
creamlaflare <antonkazachenko2002@gmail.com>
2025-03-08 20:01:10 -0800

    create AI block stub

---
+++ app/components/ai-block/ai-block.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./ai-block.module.css";
+
+export function AiBlock() {
+  return (
+    <div className={styles.camera}>
+      <img  src="../../../public/img/photoStub.jpeg" alt="AI Block" />
+    </div>
+  );
+}
+
+export default AiBlock;
--- app/components/photos-block/photos-block.tsx
+++ app/components/photos-block/photos-block.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import styles from './photos-block.module.css'
 import {WebcamBlock} from "~/components";
+import AiBlock from "~/components/ai-block/ai-block";
 
 const PhotosBlock = () => {
   return (
-    <>
+    <div className={styles.photosBlock}>
       <WebcamBlock />
       {/*<LoadingBlock />*/}
-      {/*<AiBlock />*/}
-    </>
+      <AiBlock />
+    </div>
   )
 }
 
